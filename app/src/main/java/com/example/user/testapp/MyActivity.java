7697f279813ad02a33ad97ddf986be9881371824
Leonid <nlpub0@gmail.com>
2014-11-17 18:07:48 +0300

    Add ListActivity

--- app/src/main/java/com/example/user/testapp/MyActivity.java
+++ app/src/main/java/com/example/user/testapp/MyActivity.java
@@ -1,17 +1,41 @@
 package com.example.user.testapp;
 
 import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
-public class MyActivity extends Activity {
+public class MyActivity extends ListActivity {
+    String tests[] = { "LifeCycleTest", "SingleTouchTest", "MultiTouchTest",
+            "KeyTest", "AccelerometerTest", "AssetsTest",
+            "ExternalStorageTest", "SoundPoolTest", "MediaPlayerTest",
+            "FullScreenTest", "RenderViewTest", "ShapeTest", "BitmapTest",
+            "FontTest", "SurfaceViewTest" };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, tests));
+    }
+
+    protected void onListItemClick(ListView list, View view, int position, long id){
+        super.onListItemClick(list,view,position,id);
+        String testName = tests[position];
+        try{
+            Class clazz = Class.forName("com.example.user.testapp."+testName);
+            Intent intent = new Intent(this, clazz);
+            startActivity(intent);
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
     }
 
 
